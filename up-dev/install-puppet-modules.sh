8f2dbe90c8ff9d1aa64d319888d6bc77a7a23271
Jared Godfrey <jared@updatepromise.com>
2017-05-12 15:44:54 -0700

    mysql configured, missing mysql password and credentials

--- up-dev/install-puppet-modules.sh
+++ up-dev/install-puppet-modules.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir -p /etc/puppet/modules;
 
-if [ ! -d /etc/puppet/modules/elasticsearch  ]; then
-  puppet module install elasticsearch-elasticsearch --version 0.4.0
-fi
+#if [ ! -d /etc/puppet/modules/elasticsearch  ]; then
+#  puppet module install elasticsearch-elasticsearch --version 0.4.0
+#fi
 
 if [ ! -d /etc/puppet/modules/puppetlabs-mysql  ]; then
   puppet module install puppetlabs-mysql --version 3.11.0
